e86ad6dd2621607b3bc69239f85b96a2645a379e
Ananya-0081 <as7567112@gmail.com>
2025-09-01 15:25:52 +0530

    update2 1/09/25

--- src/Array1.java
+++ src/Array1.java
@@ -1,16 +1,29 @@
 import java.util.Scanner;
 public class Array1 {
     public static void main(String[] args) {
-        int a[][]=new int [5][2];
+        int a[][]=new int [2][3];
+        int b[][]=new int [2][3];
+        int c[][]=new int [2][3];
+
         Scanner sc=new Scanner(System.in);
-        for(int i=0;i<5;i++) {
-            for (int j = 0; j < 2; j++) {
+        for(int i=0;i<2;i++) {
+            for (int j = 0; j < 3; j++) {
                 a[i][j] = sc.nextInt();
             }
         }
-        for(int i=0;i<5;i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print(a[i][j]+" ");
+        for(int i=0;i<2;i++) {
+            for (int j = 0; j < 3; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<2;i++) {
+            for (int j = 0; j < 3; j++) {
+                c[i][j] = a[i][j]+b[i][j];
+            }
+        }
+        for(int i=0;i<2;i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(c[i][j]+" ");
             }
             System.out.println();
         }
